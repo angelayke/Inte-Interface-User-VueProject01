045d1d68da31df9e4f2e4d736255606b8d51219a
angelayke <angebee2605@aol.com>
2022-12-05 18:55:29 -0500

    finir dinstaller bootstrapd ans main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 import App from './App.vue'
 import router from './router'
 
@@ -11,5 +16,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// Make BootstrapVue available throughout your project app au lieu de Vue
+app.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin app au lieu de Vue
+app.use(IconsPlugin)
 
 app.mount('#app')
