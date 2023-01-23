7e1141cf38d2fe5eba2f9e8382913443a014a481
angelayke <angebee2605@aol.com>
2023-01-22 21:16:47 -0500

    desactivation @click nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { ref,} from "vue";
-import TodosService from "../services/todo-service";
+//import { ref,} from "vue";
+//import TodosService from "../services/todo-service";
 import LogoutButton from './LogoutButton.vue';
-import filters from '../services/filter-service';
+//import filters from '../services/filter-service';
 
-const filters = new filters();
+//const filters = new filters();
 
 </script>
 
 <template>
     <nav class="navbar bg-light">
         <form class="container-fluid justify-content-start">
-        <button class="btn btn-outline-danger me-2" type="button" @click="depassees()">Tâches dépassées</button>
+        <button class="btn btn-outline-danger me-2" type="button">Tâches dépassées</button>
         <button class="btn btn-outline-primary me-2" type="button">Tâches sans limite</button>
         <button class="btn btn-outline-success me-2" type="button">Tâches encore dans les temps</button>
         <button class="btn btn-outline-secondary me-2" type="button">Tâches terminées dans les temps</button>
